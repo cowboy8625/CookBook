38f6a866955c6779a3a3616e5b8d89983e78a027
cowboy8625 <cowboy8625@yahoo.com>
2023-05-27 18:27:25 -0500

    rule110 for rust

---
+++ Languages/rule110/rule110-rust/src/main.rs
@@ -0,0 +1,52 @@
+use std::time::Duration;
+const WIDTH: usize = 100;
+
+#[derive(Debug, Clone, Copy)]
+enum Cell {
+    Allive,
+    Dead,
+}
+
+fn main() {
+    let mut grid = vec![Cell::Dead; WIDTH];
+    grid[WIDTH - 1] = Cell::Allive;
+    loop {
+        display(&grid);
+        grid = update(grid);
+        std::thread::sleep(Duration::from_millis(100));
+    }
+}
+
+fn update(old: Vec<Cell>) -> Vec<Cell> {
+    (0..WIDTH)
+        .map(|i| {
+            let a = old[(i.wrapping_sub(1).wrapping_add(old.len())) % old.len()];
+            let b = old[i];
+            let c = old[(i.wrapping_add(1).wrapping_add(old.len())) % old.len()];
+            is_alive(a, b, c)
+        })
+        .collect()
+}
+
+fn is_alive(a: Cell, b: Cell, c: Cell) -> Cell {
+    match (a, b, c) {
+        (Cell::Allive, Cell::Allive, Cell::Allive) => Cell::Dead, // 7
+        (Cell::Allive, Cell::Allive, Cell::Dead) => Cell::Allive, // 6
+        (Cell::Allive, Cell::Dead, Cell::Allive) => Cell::Allive, // 5
+        (Cell::Allive, Cell::Dead, Cell::Dead) => Cell::Dead,     // 4
+        (Cell::Dead, Cell::Allive, Cell::Allive) => Cell::Allive, // 3
+        (Cell::Dead, Cell::Allive, Cell::Dead) => Cell::Allive,   // 2
+        (Cell::Dead, Cell::Dead, Cell::Allive) => Cell::Allive,   // 1
+        (Cell::Dead, Cell::Dead, Cell::Dead) => Cell::Dead,       // 0
+    }
+}
+
+fn display(grid: &[Cell]) {
+    for cell in grid.iter() {
+        match cell {
+            Cell::Allive => print!("#"),
+            Cell::Dead => print!(" "),
+        }
+    }
+    println!();
+}
